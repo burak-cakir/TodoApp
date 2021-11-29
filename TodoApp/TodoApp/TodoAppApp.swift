fd1018df8355d4552b625441d42bb63a129df4a8
burak-cakir <burakcakir@hotmail.com.tr>
2021-11-29 23:59:19 +0300

    Holaaa

---
+++ TodoApp/TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by burak cakir on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
